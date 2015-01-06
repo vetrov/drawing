37a4c9be47c03d459c5bf341ccb4b30681c61fb8
Alexander Vetrov <alex.vetrov@gmail.com>
2015-01-06 18:48:18 +0200

    Few fixes

--- shapeController.js
+++ shapeController.js
@@ -1,14 +1,19 @@
 var shapeController = (function () {
     return {
-        addShape: function(type) {
+        addShape: function(type, callback) {
             var canvas = document.getElementById('canvas');
             var shape = document.createElement('div');
             shape.className = type;
             shape.setAttribute('shapeType', type);
 
+            if (type === 'label') {
+                shape.innerHTML = 'I am a text label';
+            }
+
             canvas.appendChild(shape);
 
             dragController.makeDraggable(shape);
+            callback.call(null, shape);
         }
     }
 
@@ -17,7 +22,9 @@ var shapeController = (function () {
 ['Rectangle', 'Square', 'Ellipse', 'Circle', 'Label'].forEach(function (shape) {
     var elem = document.getElementById('add' + shape);
     elem.onclick = function () {
-        shapeController.addShape(shape.toLowerCase());
+        shapeController.addShape(shape.toLowerCase(), function (shape) {
+            alert(shape.getAttribute('shapeType') + ' added');
+        });
     }
 });
 
